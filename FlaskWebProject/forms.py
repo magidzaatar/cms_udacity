e88ca2c8709f54eee12947529dabe19d6e6c14af
magidzaatar <54594073+magidzaatar@users.noreply.github.com>
2025-01-06 09:21:02 -0600

    Update forms.py

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -1,19 +1,25 @@
-
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-from wtforms.widgets import TextArea
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    """
+    Form for user login.
+    """
+    username = StringField('Username', validators=[DataRequired(), Length(max=64)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    author = StringField('Author', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired()])
-    image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    """
+    Form for creating or editing a post.
+    """
+    title = StringField('Title', validators=[DataRequired(), Length(max=150)])
+    author = StringField('Author', validators=[DataRequired(), Length(max=75)])
+    body = TextAreaField('Body', validators=[DataRequired(), Length(max=800)])
+    image_path = FileField('Image', validators=[
+        FileAllowed(['jpg', 'png'], 'Images only! (jpg, png)')
+    ])
     submit = SubmitField('Save')
